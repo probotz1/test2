b2b0adbe9b83d0622c6cd2a5ea8c0ab5f0eed32a
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-08 22:52:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,6 +30,6 @@ class Bot(Client):
             app = web.AppRunner(await web_server())
             await app.setup()
             bind_address = "0.0.0.0"
-            await web.TCPSite(app, bind_address, 8080).start()
+            await web.TCPSite(app, bind_address, 8050).start()
 
 Bot().run()
