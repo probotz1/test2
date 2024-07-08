bd36c857ce8d7e34ccbe5b3bc885c85b144b1e3e
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-08 22:16:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pytz import timezone
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from config import config
+from config import API_ID, API_HASH, BOT_TOKEN, WEEBHOOK
 from aiohttp import web
 from plugins import web_server
 from plugins import start
