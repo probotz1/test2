d9f26d4a1fc823452baa330ad18ad89c45a3994e
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-08 10:30:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID", "21740783")
 TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "7496680438:AAHyEZDGnIoARpfywrzQOhB27un9pja49p4")
 
+#port to run web server
+PORT = int(os.getenv('PORT', "8080"))
+
 if not all([TELEGRAM_API_ID, TELEGRAM_API_HASH, TELEGRAM_BOT_TOKEN]):
     raise ValueError("Please set the TELEGRAM_API_ID, TELEGRAM_API_HASH, and TELEGRAM_BOT_TOKEN environment variables")
