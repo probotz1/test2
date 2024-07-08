bd8ebf79386fbef7c38f5845cb75828be45d47f6
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-08 10:28:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -116,7 +116,6 @@ async def start_web_server():
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
-    PORT = int(os.getenv('PORT', 8080))
     await web.TCPSite(app, bind_address, PORT).start()
 
 if __name__ == "__main__":
