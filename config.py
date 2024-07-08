5066d1fe718df5a5689c951f3c5bc4fc5b12865a
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-08 22:30:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 class Config(object):
-#Audio-_edit_bot client Config 
-API_ID = os.getenv("API_ID", "21740783")
-API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7496680438:AAHyEZDGnIoARpfywrzQOhB27un9pja49p4")
+    #Audio-_edit_bot client Config 
+    API_ID = os.getenv("API_ID", "21740783")
+    API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7496680438:AAHyEZDGnIoARpfywrzQOhB27un9pja49p4")
 
-#port to run web server
-PORT = int(os.getenv('PORT', "8080"))
+    #port to run web server
+    PORT = int(os.getenv('PORT', "8080"))
 
-# wes response configuration     
-WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
+    # wes response configuration     
+    WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
 
 if not all([API_ID, API_HASH, BOT_TOKEN]):
     raise ValueError("Please set the API_ID, API_HASH, and BOT_TOKEN environment variables")
