c6752ee1d1095d60f8e0ad8e9d2a55b92982129a
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-14 08:11:51 +0530

    Update audio.py

--- plugins/audio.py
+++ plugins/audio.py
@@ -4,9 +4,12 @@ import subprocess
 import sys
 import math
 import time
+import progress 
 from concurrent.futures import ThreadPoolExecutor
 from flask import Flask, request, jsonify
 from pyrogram import Client, filters
+from plugins import start
+
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
