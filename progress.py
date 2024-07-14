c3475faec41ff4c15796ebecf833194a3d7b0cfd
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-14 12:11:36 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -1,30 +1,30 @@
 import time, math
-from config import Txt 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-def progress_for_pyrogram(current, total, ud_type, message, start):
+def progress(current, total, message, start_time):
     now = time.time()
-    diff = now - start
-    if round(diff % 5.00) == 0 or current == total:        
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
+    diff = now - start_time
+    if diff == 0:
+        return
 
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+    percentage = current * 100 / total
+    speed = current / diff
+    time_to_completion = (total - current) / speed
+    estimated_total_time = diff + time_to_completion
 
-        progress = "{0}{1}".format(
-            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))])
-        )            
-        tmp = progress + Txt.PROGRESS_BAR.format( 
-            round(percentage, 2),
-            humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),            
-            estimated_total_time if estimated_total_time != '' else "0 s"
-        )
-        except:
-            pass
+    progress_str = "{0:.1f}%\n[{1}{2}]".format(
+        percentage,
+        ''.join(["#" for i in range(math.floor(percentage / 10))]),
+        ''.join(["-" for i in range(10 - math.floor(percentage / 10))])
+    )
+
+    tmp = progress_str + "\n{0} of {1}\nSpeed: {2}/s\nETA: {3}".format(
+        humanbytes(current),
+        humanbytes(total),
+        humanbytes(speed),
+        time.strftime("%H:%M:%S", time.gmtime(estimated_total_time))
+    )
+
+    try:
+        message.edit_text(text="Downloading...\n" + tmp)
+    except:
+        pass
