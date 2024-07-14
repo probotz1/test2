adc7cdf22572d980d858daf5e88ea96009e8a0d8
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-14 08:01:25 +0530

    Update audio.py

--- plugins/audio.py
+++ plugins/audio.py
@@ -41,35 +41,6 @@ def trim_video(input_file, start_time, end_time, output_file):
     ]
     return run_command(command)
 
-def progress(current, total, message, start_time):
-    now = time.time()
-    diff = now - start_time
-    if diff == 0:
-        return
-
-    percentage = current * 100 / total
-    speed = current / diff
-    time_to_completion = (total - current) / speed
-    estimated_total_time = diff + time_to_completion
-
-    progress_str = "{0:.1f}%\n[{1}{2}]".format(
-        percentage,
-        ''.join(["#" for i in range(math.floor(percentage / 10))]),
-        ''.join(["-" for i in range(10 - math.floor(percentage / 10))])
-    )
-
-    tmp = progress_str + "\n{0} of {1}\nSpeed: {2}/s\nETA: {3}".format(
-        humanbytes(current),
-        humanbytes(total),
-        humanbytes(speed),
-        time.strftime("%H:%M:%S", time.gmtime(estimated_total_time))
-    )
-
-    try:
-        message.edit_text(text="Downloading...\n" + tmp)
-    except:
-        pass
-
 @Client.on_message(filters.command("start"))
 def start(client, message):
     start_text = "Hello! Send me a video to process. Use /help to see available commands."
