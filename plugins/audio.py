4bcfd63105787878fc3b8f78f26a652dca3bb5d0
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-15 19:37:12 +0530

    Update audio.py

--- plugins/audio.py
+++ plugins/audio.py
@@ -9,8 +9,6 @@ from concurrent.futures import ThreadPoolExecutor
 from flask import Flask, request, jsonify
 from pyrogram import Client, filters
 from plugins import start
-from helper.utils import progress_for_pyrogram, convert, humanbytes
-from helper.database import db
 from pyrogram.errors import FloodWait
 
 app = Flask(__name__)
