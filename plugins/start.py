812ee1e0f8d722ee5dc7dee73c7e6158fbb3b690
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-15 13:20:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,10 +1,31 @@
 import os
 import time 
 import math
+import random
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import Txt
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply, CallbackQuery
+from config import Config, Txt
 
+@Client.on_message(filters.private & filters.command("start"))
+async def start(client, message):
+    user = message.from_user
+    await db.add_user(client, message)                
+    button = InlineKeyboardMarkup([[
+        InlineKeyboardButton("Update Channel", url="https://t.me/Anime_Warrior_Tamil"),
+        InlineKeyboardButton("Support Group", url="https://t.me/+NITVxLchQhYzNGZl")
+    ],[
+        InlineKeyboardButton("Help", callback_data="help"),
+        InlineKeyboardButton("About", callback_data="about")
+    ],[
+        InlineKeyboardButton("Close", callback_data="close")
+    ]])
+    message.reply_text(start_text, reply_markup=keyboard)
+    
+    if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
+    else:
+        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+   
 @Client.on_message(filters.command("start"))
 def start(client, message):
     start_text = Txt.START_TXT,
