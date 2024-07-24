4c8d6d8adcb4d6932a8fd38ce3520c717c77d3f5
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-24 09:11:55 +0530

    Update extractor.py

--- plugins/extractor.py
+++ plugins/extractor.py
@@ -28,3 +28,12 @@ async def confirm_dwnld(client, message):
             "Invalid Media",
             quote=True
         )
+
+    elif data == "close":
+        try:
+            await query.message.delete()
+            await query.message.reply_to_message.delete()
+            await query.message.continue_propagation()
+        except:
+            await query.message.delete()
+            await query.message.continue_propagation()
