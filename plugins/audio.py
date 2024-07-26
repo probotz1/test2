8377f3af1879bd4dc169a2b696829b3aae4f4f31
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-26 21:40:07 +0530

    Update audio.py

--- plugins/audio.py
+++ plugins/audio.py
@@ -131,11 +131,5 @@ async def callback_handler(client, callback_query):
     if data == "handle_remove_audio":
         await callback_query.answer()
         await handle_remove_audio(client, callback_query.message)
-    elif data == "handle_trim_video":
-        await callback_query.answer()
-        await callback_query.message.reply_text("Please use the command in the format: /trim_video <start_time> <end_time>.\nExample: /trim_video 00:00:10 00:00:20")
-    elif data == "download_file":
-        await callback_query.answer()
-        await extractor.extract_audio(client, callback_query.message)
-    elif data == "close":
-        await callback_query.message.delete()
+        pass
+
